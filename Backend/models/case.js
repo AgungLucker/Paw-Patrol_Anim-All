6d01e52434d23522a8616034e5c249119dd2986d
AgungLucker <iniabi838@gmail.com>
2025-05-27 17:37:06 +0700

    feat: add reportID and backend validation

--- Backend/models/case.js
+++ Backend/models/case.js
@@ -1,11 +1,17 @@
 
 const mongoose = require('mongoose');
+const IDCounter = require('./idCounter'); 
 
 // atribut data di database mongodb
 const caseSchema = new mongoose.Schema({
+    reportID: {
+        type: Number,
+        unique: true,
+    },
     name: {
         type: String,
-        required: true
+        required: true,
+        minlength: 2
     },
     speciesType: {
         type: String,
@@ -30,11 +36,13 @@ const caseSchema = new mongoose.Schema({
     },
     ownerContact: {
         type: String,
-        required: true
+        required: true,
+        minlength: 10,
+        maxlength: 13
     },
     lostDate: {
         type: Date,
-        default: Date.now
+        required: true
     },
     postDate: {
         type: Date,
@@ -43,7 +51,7 @@ const caseSchema = new mongoose.Schema({
     // kalau close case statusnya jadi found
     status: {
         type: String,
-        enum: ['lost', 'found'],
+        enum: ['lost', 'found', 'returned'],
         default: 'lost',
         required: true
     }, 
@@ -52,7 +60,7 @@ const caseSchema = new mongoose.Schema({
         required: true,
         unique: true 
     },
-    // foundArea, foundDate diinisialisasi di PUT,
+    // foundArea, foundDate diinisialisasi di Put (jika ada)
     foundArea: {
         type: String,
         required: false
@@ -63,6 +71,22 @@ const caseSchema = new mongoose.Schema({
     }
 }, {
   timestamps: true 
+});
+
+// buat reportID secara incremental
+caseSchema.pre('save', async function(next) {
+    if (this.isNew && !this.reportID) {
+        try {
+            const nextId = await IDCounter.getNextSequence('caseID');
+            this.reportID = nextId;
+            next();
+        } catch (err) {
+            console.error("Error generating reportID:", err);
+            next(err);
+        }
+    } else {
+        next();
+    }
 });
 
 module.exports = mongoose.model('Case', caseSchema);
