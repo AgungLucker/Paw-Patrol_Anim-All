fd3b4c3979e9422a96a53f08ef4196c4d4524799
AgungLucker <iniabi838@gmail.com>
2025-06-08 21:08:36 +0700

    fix: db backend

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    const db = await  mongoose.connect(process.env.MONGODB_URI, {
       // useNewUrlParser: true,
       // useUnifiedTopology: true,
     });
@@ -17,7 +17,7 @@ const connectDB = async () => {
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
-    process.exit(1);
+    throw err;
   }
 };
 
